f93bc064dc276bed4f3c856405e5da6a4adf70e0
Philip Lowe <philip.lowe94@gmail.com>
2021-06-24 17:50:39 -0400

    finished tag actions

--- frontend/actions/tag_actions.js
+++ frontend/actions/tag_actions.js
@@ -11,3 +11,39 @@ const receiveTags = (tag) => ({
     type: RECEIVE_TAGS,
     tags
 })
+
+const receiveTag = (tag) => ({
+    type: RECEIVE_TAG,
+    tag
+})
+
+const removeTag = (tagId) => ({
+    type: REMOVE_TAG,
+    tagId
+})
+
+//thunk action creators
+export const fetchTags = () => dispatch => (
+    TagApiUtil.fetchTags()
+    .then(tags => dispatch(receiveTags(tags)))
+);
+
+export const fetchTag = (tagId) => dispatch => (
+    TagApiUtil.fetchTag(tagId)
+    .then(tag => dispatch(receiveTag(tag)))
+);
+
+export const createTag = (tag) => dispatch => (
+    TagApiUtil.createTag(tag)
+    .then(tag => dispatch(receiveTag(tag)))
+)
+
+export const updateTag = (tag) => dispatch => (
+    TagApiUtil.updateTag(tag)
+    .then(tag=> dispatch(receiveTag(tag)))
+)
+
+export const deleteTag = (tagId) => dispatch => (
+    TagApiUtil.deleteTag(tagId)
+    .then(() => dispatch(removeTag(tagId)))
+)
